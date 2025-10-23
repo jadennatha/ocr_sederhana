cd97726e3ab59d3c7f83eb589cdb7b130aa5d013
jadennatha <144076853+jadennatha@users.noreply.github.com>
2025-10-23 14:46:27 +0700

    UTS: Selesai Soal 1- ListTile dan Navigasi Balik

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,16 +8,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Menu Utama')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => const ScanScreen()),
-            );
-          },
-          child: const Text('Mulai Scan Teks'),
-        ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.camera_alt),
+            title: const Text('Mulai Scan Teks'),
+            subtitle: const Text('Scan teks dari kamera'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const ScanScreen()),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 class ResultScreen extends StatelessWidget {
   final String ocrText;
@@ -8,6 +9,17 @@ class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            ),
+            (route) => false,
+          );
+        },
+        child: const Icon(Icons.home),
+      ),
       appBar: AppBar(title: const Text('Hasil OCR')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
